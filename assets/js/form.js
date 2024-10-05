818d37595dcc99dde7ecac50006bb8569f0b7be2
Colton Doling <cmdoling@gmail.com>
2024-10-05 14:55:56 -0400

    added code to form.js as well as a couple more elements to both HTML files

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,30 @@
 // TODO: Create a variable that selects the form element
-
+const getForm = document.querySelector('form');
+const getUsername = document.getElementById('username');
+const getTitle = document.getElementById('title');
+const getContent = document.getElementById('content');
+const submitButton = document.getElementById('submit');
+const error = document.getElementById('error');
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
+const formSubmit = function () {
+    const user = {
+        username: getUsername.value,
+        title: getTitle.value,
+        content: getContent.value
+    };
 
+    if (getUsername.value === '' || getTitle.value === '' || getContent.value === '') {
+        error.textContent = 'Please complete the form'
+    } else {
+        localStorage.setItem('user', JSON.stringify(user));
+        redirectPage('../blog.html');
+        getUsername.value = '';
+        getTitle.value = '';
+        getContent.value = '';
+    };
+};
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    formSubmit();
+});
